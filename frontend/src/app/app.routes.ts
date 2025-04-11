aa0ca92cbf36387e10ce69670ee1e07a8b333d5d
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-11 11:40:59 -0230

    Created component for previous trees

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { InputNumbersComponent } from './components/input-numbers/input-numbers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'enter-numbers', pathMatch: 'full' },
+  { path: 'enter-numbers', component: InputNumbersComponent },
+  {
+    path: 'tree-visual',
+    loadComponent: () =>
+      import('./components/tree-visual/tree-visual.component').then(
+        (m) => m.TreeVisualComponent
+      ),
+  },
+  {
+    path: 'previous-trees',
+    loadComponent: () =>
+      import('./components/previous-trees/previous-trees.component').then(
+        (m) => m.PreviousTreesComponent
+      ),
+  }
+];
+
---
+++ frontend/src/app/components/previous-trees/previous-trees.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-previous-trees',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: 'previous-trees.component.html',
+  styleUrls: ['previous-trees.component.scss']
+})
+export class PreviousTreesComponent{}
